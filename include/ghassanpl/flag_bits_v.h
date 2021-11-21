e1c784cf01a8fd28326ad4574d6827ac32659d5d
Ghassan Al-Mashareqa <kronikarz@gmail.com>
2021-11-21 23:16:10 +0100

    * moved some headers not ready to use * added the MPL header to each file

--- include/ghassanpl/flag_bits_v.h
+++ include/ghassanpl/flag_bits_v.h
@@ -1,7 +1,7 @@
-/// Copyright 2017-2020 Ghassan.pl
-/// Usage of the works is permitted provided that this instrument is retained with
-/// the works, so that any entity that uses the works is notified of this instrument.
-/// DISCLAIMER: THE WORKS ARE WITHOUT WARRANTY.
+/// This Source Code Form is subject to the terms of the Mozilla Public
+/// License, v. 2.0. If a copy of the MPL was not distributed with this
+/// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 #pragma once
 
 #include <type_traits>
