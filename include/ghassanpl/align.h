c033a2a86e8c81af6dfc0a71973b4b79366b1d36
Ghassan Al-Mashareqa <kronikarz@gmail.com>
2021-11-27 05:16:18 +0100

    * changed the order of directions in align enumerators (left_bottom -> bottom_left) to better reflect the english way of saying it * added aliases center_top = top_center, center_bottom = bottom_center and center_middle = middle_center, as well as center = middle for vertical * added british spelling aliases in the enums (centre = center) * added arrays of alignments in integral order and in clock orders * added arrays of names for align (both reflective and natural versions) * added to_name functions to convert alignments to strings * moved align masks outside of their respective enums into a detail namespace * added to_opposite, rotate_clockwise, rotate_counter_clockwise and flip_horizontally/vertically for alignments * fixed a bug with to_vertical and to_horizontal being flipped * added all the align tests I could think of, and made sure the tests stay C++20 as well

--- include/ghassanpl/align.h
+++ include/ghassanpl/align.h
@@ -14,21 +14,17 @@ namespace ghassanpl
 	enum class horizontal_align
 	{
 		left = 0,
-		center = 1,
+		center = 1, centre = 1,
 		right = 2,
 		justify = 3,
-
-		mask = left | center | right
 	};
 
 	enum class vertical_align
 	{
 		top = 0,
-		middle = 4,
+		middle = 4, center = 4, centre = 4,
 		bottom = 8,
 		justify = 12,
-
-		mask = top | middle | bottom
 	};
 
 	enum class align;
@@ -38,33 +34,63 @@ namespace ghassanpl
 
 	enum class align
 	{
-		left_top = int(horizontal_align::left | vertical_align::top),
-		center_top = int(horizontal_align::center | vertical_align::top),
-		right_top = int(horizontal_align::right | vertical_align::top),
+		top_left = int(horizontal_align::left | vertical_align::top),
+		top_center = int(horizontal_align::center | vertical_align::top), top_centre = top_center, center_top = top_center, centre_top = top_center,
+		top_right = int(horizontal_align::right | vertical_align::top),
 
-		left_middle = int(horizontal_align::left | vertical_align::middle),
-		center_middle = int(horizontal_align::center | vertical_align::middle),
-		right_middle = int(horizontal_align::right | vertical_align::middle),
+		middle_left = int(horizontal_align::left | vertical_align::middle), center_left = middle_left, centre_left = middle_left,
+		middle_center = int(horizontal_align::center | vertical_align::middle), center = middle_center, centre = middle_center,
+		middle_right = int(horizontal_align::right | vertical_align::middle), center_right = middle_right, centre_right = middle_right,
 
-		left_bottom = int(horizontal_align::left | vertical_align::bottom),
-		center_bottom = int(horizontal_align::center | vertical_align::bottom),
-		right_bottom = int(horizontal_align::right | vertical_align::bottom),
+		bottom_left = int(horizontal_align::left | vertical_align::bottom),
+		bottom_center = int(horizontal_align::center | vertical_align::bottom), bottom_centre = bottom_center, center_bottom = bottom_center, centre_bottom = bottom_center,
+		bottom_right = int(horizontal_align::right | vertical_align::bottom),
 	};
+
+	constexpr inline align aligns_in_order[] = { align::top_left, align::top_center, align::top_right, align::middle_left, align::middle_center, align::middle_right, align::bottom_left, align::bottom_center, align::bottom_right };
+	constexpr inline align aligns_clockwise[] = { align::top_left, align::top_center, align::top_right, align::middle_right, align::bottom_right, align::bottom_center, align::bottom_left, align::middle_left };
+	constexpr inline align aligns_counter_clockwise[] = { align::top_left, align::middle_left, align::bottom_left, align::bottom_center, align::bottom_right, align::middle_right, align::top_right, align::top_center };
+
+	constexpr inline horizontal_align horizontal_aligns_in_order[] = { horizontal_align::left, horizontal_align::center, horizontal_align::right };
+	constexpr inline vertical_align vertical_aligns_in_order[] = { vertical_align::top, vertical_align::middle, vertical_align::bottom };
 
 	constexpr inline align& operator|=(align& first, horizontal_align second) { return first = align(int(first) | int(second)); }
 	constexpr inline align& operator|=(align& first, vertical_align second) { return first = align(int(first) | int(second)); }
 
-	constexpr inline const char* horizontal_align_names[] = { "left", "center", "right", "justify_horizontal" };
+	constexpr inline const char* horizontal_align_names[] = { "left", "center", "right", "justify_horizontal", "center", "", "", "", "right", "", "", "", "justify_horizontal" };
 	constexpr inline const char* vertical_align_names[] = { "top", "middle", "bottom", "justify_vertical", "middle", "", "", "", "bottom", "", "", "", "justify_vertical" };
-		
-	constexpr inline vertical_align to_vertical(horizontal_align alignment) { return vertical_align{ (int(alignment) & int(horizontal_align::mask)) >> 2 }; }
+	constexpr inline const char* align_names[] =         { "top_left", "top_center", "top_right", "", "middle_left", "middle_center", "middle_right", "", "bottom_left", "bottom_center", "bottom_right", "", "" };
+	constexpr inline const char* align_names_natural[] = { "top left", "center top", "top right", "", "middle left", "center", "middle right", "", "bottom left", "center bottom", "bottom right", "", "" };
+
+	constexpr inline const char* to_name(horizontal_align h) { return horizontal_align_names[int(h)]; }
+	constexpr inline const char* to_name(vertical_align v) { return vertical_align_names[int(v)]; }
+	constexpr inline const char* to_name(align a) { return align_names[int(a)]; }
+	constexpr inline const char* to_natural_name(align a) { return align_names_natural[int(a)]; }
+
+	namespace detail
+	{
+		static inline constexpr auto horizontal_align_mask = int(horizontal_align::left) | int(horizontal_align::center) | int(horizontal_align::right);
+		static inline constexpr auto vertical_align_mask = int(vertical_align::top) | int(vertical_align::middle) | int(vertical_align::bottom);
+	}
+
+	constexpr inline vertical_align to_vertical(horizontal_align alignment) { return vertical_align{ (int(alignment) & detail::horizontal_align_mask) << 2 }; }
 	constexpr inline vertical_align to_vertical(vertical_align alignment) { return alignment; }
-	constexpr inline horizontal_align to_horizontal(vertical_align alignment) { return horizontal_align{ (int(alignment) & int(vertical_align::mask)) << 2 }; }
+	constexpr inline horizontal_align to_horizontal(vertical_align alignment) { return horizontal_align{ (int(alignment) & detail::vertical_align_mask) >> 2 }; }
 	constexpr inline horizontal_align to_horizontal(horizontal_align alignment) { return alignment; }
-	constexpr inline vertical_align vertical_from(align alignment) { return vertical_align{ (int(alignment) & int(vertical_align::mask)) }; }
-	constexpr inline horizontal_align horizontal_from(align alignment) { return horizontal_align{ (int(alignment) & int(horizontal_align::mask)) }; }
-	constexpr inline align only_vertical(align alignment) { return align{ (int(alignment) & int(vertical_align::mask)) }; }
-	constexpr inline align only_horizontal(align alignment) { return align{ (int(alignment) & int(horizontal_align::mask)) }; }
+	constexpr inline vertical_align vertical_from(align alignment) { return vertical_align{ (int(alignment) & detail::vertical_align_mask) }; }
+	constexpr inline horizontal_align horizontal_from(align alignment) { return horizontal_align{ (int(alignment) & detail::horizontal_align_mask) }; }
+	constexpr inline align only_vertical(align alignment) { return align{ (int(alignment) & detail::vertical_align_mask) }; }
+	constexpr inline align only_horizontal(align alignment) { return align{ (int(alignment) & detail::horizontal_align_mask) }; }
+
+	constexpr inline vertical_align to_opposite(vertical_align alignment) { return vertical_align{ ((2 - (int(alignment) >> 2)) & detail::horizontal_align_mask) << 2 }; }
+	constexpr inline horizontal_align to_opposite(horizontal_align alignment) { return horizontal_align{ (2 - int(alignment)) & detail::horizontal_align_mask }; }
+	constexpr inline align to_opposite(align alignment) { return to_opposite(vertical_from(alignment)) | to_opposite(horizontal_from(alignment)); }
+
+	constexpr inline align rotate_clockwise(align alignment) { return to_horizontal(to_opposite(vertical_from(alignment))) | to_vertical(horizontal_from(alignment)); }
+	constexpr inline align rotate_counter_clockwise(align alignment) { return to_horizontal(vertical_from(alignment)) | to_vertical(to_opposite(horizontal_from(alignment))); }
+
+	constexpr inline align flip_horizontally(align alignment) { return vertical_from(alignment) | to_opposite(horizontal_from(alignment)); }
+	constexpr inline align flip_vertically(align alignment) { return to_opposite(vertical_from(alignment)) | horizontal_from(alignment); }
 
 	template <typename T>
 	inline constexpr T align_axis(const T& width, const T& max_width, horizontal_align align) {
---
+++ tests/align_tests.cpp
@@ -0,0 +1,287 @@
+#include "../include/ghassanpl/align.h"
+
+#include <set>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <magic_enum.hpp>
+#if __has_include(<experimental/generator>)
+#include <experimental/generator>
+
+template <typename RANGE1, typename RANGE2>
+auto cartesian_product(RANGE1&& r1, RANGE2&& r2) -> std::experimental::generator<std::pair<std::ranges::range_value_t<RANGE1>, std::ranges::range_value_t<RANGE2>>>
+{
+  for (auto& e1 : r1)
+    for (auto& e2 : r2)
+      co_yield std::pair{ e1, e2 };
+}
+
+#else
+template <typename RANGE1, typename RANGE2>
+auto cartesian_product(RANGE1&& r1, RANGE2&& r2)
+{
+  std::vector<std::pair<std::ranges::range_value_t<RANGE1>, std::ranges::range_value_t<RANGE2>>> result;
+  for (auto&& e1 : std::forward<RANGE1>(r1))
+    for (auto&& e2 : std::forward<RANGE2>(r2))
+      result.emplace_back(std::forward<std::ranges::range_value_t<RANGE1>>(e1), std::forward<std::ranges::range_value_t<RANGE2>>(e2));
+  return result;
+}
+#endif
+#include <ranges>
+
+#include <gtest/gtest.h>
+
+using namespace ghassanpl;
+
+const std::set<align> all_aligns{
+  align::top_left, align::middle_left, align::bottom_left,
+  align::top_center, align::middle_center, align::bottom_center,
+  align::top_right, align::middle_right, align::bottom_right,
+};
+
+const std::set<horizontal_align> horizontal_aligns{
+  horizontal_align::left, horizontal_align::center, horizontal_align::right
+};
+
+const std::set<vertical_align> vertical_aligns{
+  vertical_align::top, vertical_align::bottom, vertical_align::middle
+};
+
+TEST(alignment, basics_work)
+{
+  align a{ align::top_left };
+  EXPECT_EQ(horizontal_align::left | vertical_align::bottom, a |= vertical_align::bottom);
+
+  ASSERT_EQ(vertical_aligns.size(), 3);
+
+  ASSERT_EQ(vertical_aligns.size(), 3);
+
+  ASSERT_EQ(vertical_aligns.size(), 3);
+
+  auto range = cartesian_product(vertical_aligns, horizontal_aligns);
+  auto other_range = range | std::ranges::views::transform([](auto p) { return p.first | p.second; });
+  ASSERT_EQ(all_aligns, (std::set<align> { other_range.begin(), other_range.end() }));
+}
+
+TEST(alignment, names_work)
+{
+#define E(t, a) EXPECT_EQ(#a, t##_names[int(t::a)])
+  E(horizontal_align, left);
+  E(horizontal_align, center);
+  E(horizontal_align, right);
+  E(vertical_align, top);
+  E(vertical_align, middle);
+  E(vertical_align, bottom);
+
+  E(align, top_left);
+  E(align, middle_left);
+  E(align, bottom_left);
+  E(align, top_center);
+  E(align, middle_center);
+  E(align, bottom_center);
+  E(align, top_right);
+  E(align, middle_right);
+  E(align, bottom_right);
+
+  for (auto v : vertical_aligns_in_order)
+  {
+    EXPECT_EQ(magic_enum::enum_name(v), to_name(v));
+  }
+  for (auto h : horizontal_aligns_in_order)
+  {
+    EXPECT_EQ(magic_enum::enum_name(h), to_name(h));
+  }
+  for (auto a : aligns_in_order)
+  {
+    EXPECT_EQ(magic_enum::enum_name(a), to_name(a));
+  }
+}
+
+TEST(alignment, conversions_work)
+{
+  for (auto v : vertical_aligns_in_order)
+  {
+    ASSERT_EQ(to_vertical(to_horizontal(v)), v);
+  }
+  for (auto h : horizontal_aligns_in_order)
+  {
+    ASSERT_EQ(to_horizontal(to_vertical(h)), h);
+  }
+
+  EXPECT_EQ(to_opposite(horizontal_align::left), horizontal_align::right);
+  EXPECT_EQ(to_opposite(horizontal_align::center), horizontal_align::center);
+  EXPECT_EQ(to_opposite(horizontal_align::right), horizontal_align::left);
+
+  EXPECT_EQ(to_opposite(vertical_align::top), vertical_align::bottom);
+  EXPECT_EQ(to_opposite(vertical_align::middle), vertical_align::middle);
+  EXPECT_EQ(to_opposite(vertical_align::bottom), vertical_align::top);
+
+  EXPECT_EQ(to_opposite(align::top_left), align::bottom_right);
+  EXPECT_EQ(to_opposite(align::top_center), align::bottom_center);
+  EXPECT_EQ(to_opposite(align::top_right), align::bottom_left);
+  EXPECT_EQ(to_opposite(align::middle_left), align::middle_right);
+  EXPECT_EQ(to_opposite(align::center), align::center);
+  EXPECT_EQ(to_opposite(align::middle_right), align::middle_left);
+  EXPECT_EQ(to_opposite(align::bottom_left), align::top_right);
+  EXPECT_EQ(to_opposite(align::bottom_center), align::top_center);
+  EXPECT_EQ(to_opposite(align::bottom_right), align::top_left);
+
+  EXPECT_EQ(rotate_clockwise(align::top_left), align::top_right);
+  EXPECT_EQ(rotate_clockwise(align::top_center), align::middle_right);
+  EXPECT_EQ(rotate_clockwise(align::top_right), align::bottom_right);
+  EXPECT_EQ(rotate_clockwise(align::middle_left), align::top_center);
+  EXPECT_EQ(rotate_clockwise(align::center), align::center);
+  EXPECT_EQ(rotate_clockwise(align::middle_right), align::bottom_center);
+  EXPECT_EQ(rotate_clockwise(align::bottom_left), align::top_left);
+  EXPECT_EQ(rotate_clockwise(align::bottom_center), align::middle_left);
+  EXPECT_EQ(rotate_clockwise(align::bottom_right), align::bottom_left);
+
+  EXPECT_EQ(rotate_counter_clockwise(align::top_left), align::bottom_left);
+  EXPECT_EQ(rotate_counter_clockwise(align::top_center), align::middle_left);
+  EXPECT_EQ(rotate_counter_clockwise(align::top_right), align::top_left);
+  EXPECT_EQ(rotate_counter_clockwise(align::middle_left), align::bottom_center);
+  EXPECT_EQ(rotate_counter_clockwise(align::center), align::center);
+  EXPECT_EQ(rotate_counter_clockwise(align::middle_right), align::top_center);
+  EXPECT_EQ(rotate_counter_clockwise(align::bottom_left), align::bottom_right);
+  EXPECT_EQ(rotate_counter_clockwise(align::bottom_center), align::middle_right);
+  EXPECT_EQ(rotate_counter_clockwise(align::bottom_right), align::top_right);
+
+  EXPECT_EQ(flip_vertically(align::top_left), align::bottom_left);
+  EXPECT_EQ(flip_vertically(align::top_center), align::bottom_center);
+  EXPECT_EQ(flip_vertically(align::top_right), align::bottom_right);
+  EXPECT_EQ(flip_vertically(align::middle_left), align::middle_left);
+  EXPECT_EQ(flip_vertically(align::center), align::center);
+  EXPECT_EQ(flip_vertically(align::middle_right), align::middle_right);
+  EXPECT_EQ(flip_vertically(align::bottom_left), align::top_left);
+  EXPECT_EQ(flip_vertically(align::bottom_center), align::top_center);
+  EXPECT_EQ(flip_vertically(align::bottom_right), align::top_right);
+
+  EXPECT_EQ(flip_horizontally(align::top_left), align::top_right);
+  EXPECT_EQ(flip_horizontally(align::top_center), align::top_center);
+  EXPECT_EQ(flip_horizontally(align::top_right), align::top_left);
+  EXPECT_EQ(flip_horizontally(align::middle_left), align::middle_right);
+  EXPECT_EQ(flip_horizontally(align::center), align::center);
+  EXPECT_EQ(flip_horizontally(align::middle_right), align::middle_left);
+  EXPECT_EQ(flip_horizontally(align::bottom_left), align::bottom_right);
+  EXPECT_EQ(flip_horizontally(align::bottom_center), align::bottom_center);
+  EXPECT_EQ(flip_horizontally(align::bottom_right), align::bottom_left);
+
+  EXPECT_EQ(to_vertical(horizontal_align::left), vertical_align::top);
+  EXPECT_EQ(to_vertical(horizontal_align::center), vertical_align::middle);
+  EXPECT_EQ(to_vertical(horizontal_align::right), vertical_align::bottom);
+  EXPECT_EQ(horizontal_align::left, to_horizontal(vertical_align::top));
+  EXPECT_EQ(horizontal_align::center, to_horizontal(vertical_align::middle));
+  EXPECT_EQ(horizontal_align::right, to_horizontal(vertical_align::bottom));
+
+  EXPECT_EQ(to_vertical(vertical_align::top), vertical_align::top);
+  EXPECT_EQ(to_vertical(vertical_align::middle), vertical_align::middle);
+  EXPECT_EQ(to_vertical(vertical_align::bottom),  vertical_align::bottom);
+  EXPECT_EQ(horizontal_align::left, to_horizontal(horizontal_align::left));
+  EXPECT_EQ(horizontal_align::center, to_horizontal(horizontal_align::center));
+  EXPECT_EQ(horizontal_align::right, to_horizontal(horizontal_align::right));
+
+  EXPECT_EQ(vertical_from(align::top_center), vertical_align::top);
+  EXPECT_EQ(vertical_from(align::middle_center), vertical_align::middle);
+  EXPECT_EQ(vertical_from(align::bottom_center), vertical_align::bottom);
+  EXPECT_EQ(vertical_from(align::top_left), vertical_align::top);
+  EXPECT_EQ(vertical_from(align::middle_left), vertical_align::middle);
+  EXPECT_EQ(vertical_from(align::bottom_left), vertical_align::bottom);
+  EXPECT_EQ(vertical_from(align::top_right), vertical_align::top);
+  EXPECT_EQ(vertical_from(align::middle_right), vertical_align::middle);
+  EXPECT_EQ(vertical_from(align::bottom_right), vertical_align::bottom);
+
+  EXPECT_EQ(horizontal_from(align::top_center), horizontal_align::center);
+  EXPECT_EQ(horizontal_from(align::middle_center), horizontal_align::center);
+  EXPECT_EQ(horizontal_from(align::bottom_center), horizontal_align::center);
+  EXPECT_EQ(horizontal_from(align::top_left), horizontal_align::left);
+  EXPECT_EQ(horizontal_from(align::middle_left), horizontal_align::left);
+  EXPECT_EQ(horizontal_from(align::bottom_left), horizontal_align::left);
+  EXPECT_EQ(horizontal_from(align::top_right), horizontal_align::right);
+  EXPECT_EQ(horizontal_from(align::middle_right), horizontal_align::right);
+  EXPECT_EQ(horizontal_from(align::bottom_right), horizontal_align::right);
+
+  EXPECT_EQ(only_vertical(align::top_center), align::top_left);
+  EXPECT_EQ(only_vertical(align::middle_center), align::middle_left);
+  EXPECT_EQ(only_vertical(align::bottom_center), align::bottom_left);
+  EXPECT_EQ(only_vertical(align::top_left), align::top_left);
+  EXPECT_EQ(only_vertical(align::middle_left), align::middle_left);
+  EXPECT_EQ(only_vertical(align::bottom_left), align::bottom_left);
+  EXPECT_EQ(only_vertical(align::top_right), align::top_left);
+  EXPECT_EQ(only_vertical(align::middle_right), align::middle_left);
+  EXPECT_EQ(only_vertical(align::bottom_right), align::bottom_left);
+
+  EXPECT_EQ(only_horizontal(align::top_center), align::top_center);
+  EXPECT_EQ(only_horizontal(align::middle_center), align::top_center);
+  EXPECT_EQ(only_horizontal(align::bottom_center), align::top_center);
+  EXPECT_EQ(only_horizontal(align::top_left), align::top_left);
+  EXPECT_EQ(only_horizontal(align::middle_left), align::top_left);
+  EXPECT_EQ(only_horizontal(align::bottom_left), align::top_left);
+  EXPECT_EQ(only_horizontal(align::top_right), align::top_right);
+  EXPECT_EQ(only_horizontal(align::middle_right), align::top_right);
+  EXPECT_EQ(only_horizontal(align::bottom_right), align::top_right);
+}
+
+TEST(alignment, axis_alignments_work)
+{
+  EXPECT_EQ(align_axis(10, 100, horizontal_align::left), 0);
+  EXPECT_EQ(align_axis(10.0, 100.0, horizontal_align::center), 45.0);
+  EXPECT_EQ(align_axis(10.0f, 100.0f, horizontal_align::right), 90.0f);
+
+  EXPECT_EQ(align_axis(10, 100, vertical_align::top), 0);
+  EXPECT_EQ(align_axis(10, 100, vertical_align::middle), 45);
+  EXPECT_EQ(align_axis(10, 100, vertical_align::bottom), 90);
+
+  EXPECT_EQ(align_axis(0, 0, horizontal_align::left), 0);
+  EXPECT_EQ(align_axis(0, 0, horizontal_align::center), 0);
+  EXPECT_EQ(align_axis(0, 0, horizontal_align::right), 0);
+
+  EXPECT_EQ(align_axis(100.0, 100.0, horizontal_align::left), 0.0);
+  EXPECT_EQ(align_axis(100.0, 100.0, horizontal_align::center), 0.0);
+  EXPECT_EQ(align_axis(100.0, 100.0, horizontal_align::right), 0.0);
+
+  EXPECT_EQ(align_axis(10.0f, INFINITY, horizontal_align::left), 0.0f);
+  EXPECT_EQ(align_axis(10.0f, INFINITY, horizontal_align::center), INFINITY);
+  EXPECT_EQ(align_axis(10.0f, INFINITY, horizontal_align::right), INFINITY);
+
+  EXPECT_EQ(align_axis(100.0, 10.0, horizontal_align::left), 0.0);
+  EXPECT_EQ(align_axis(100.0, 10.0, horizontal_align::center), -45.0);
+  EXPECT_EQ(align_axis(100.0, 10.0, horizontal_align::right), -90.0);
+
+  EXPECT_EQ(align_axis(INFINITY, 10.0f, horizontal_align::left), 0.0f);
+  EXPECT_EQ(align_axis(INFINITY, 10.0f, horizontal_align::center), -INFINITY);
+  EXPECT_EQ(align_axis(INFINITY, 10.0f, horizontal_align::right), -INFINITY);
+}
+
+TEST(alignment, justify_doesnt_break_anything)
+{
+  EXPECT_FALSE(horizontal_aligns.contains(horizontal_align::justify));
+  EXPECT_FALSE(vertical_aligns.contains(vertical_align::justify));
+
+  EXPECT_FALSE(all_aligns.contains(horizontal_align::justify | vertical_align::top));
+  EXPECT_FALSE(all_aligns.contains(horizontal_align::justify | vertical_align::middle));
+  EXPECT_FALSE(all_aligns.contains(horizontal_align::justify | vertical_align::bottom));
+
+  EXPECT_FALSE(all_aligns.contains(vertical_align::justify | horizontal_align::left));
+  EXPECT_FALSE(all_aligns.contains(vertical_align::justify | horizontal_align::center));
+  EXPECT_FALSE(all_aligns.contains(vertical_align::justify | horizontal_align::right));
+
+  EXPECT_EQ(to_horizontal(vertical_align::justify), horizontal_align::justify);
+  EXPECT_EQ(to_vertical(horizontal_align::justify), vertical_align::justify);
+
+  EXPECT_EQ(only_horizontal(vertical_align::justify | horizontal_align::left), align::top_left);
+  EXPECT_EQ(only_horizontal(vertical_align::justify | horizontal_align::center), align::top_center);
+  EXPECT_EQ(only_horizontal(vertical_align::justify | horizontal_align::right), align::top_right);
+
+  EXPECT_EQ(vertical_from(vertical_align::justify | horizontal_align::justify), vertical_align::justify);
+  EXPECT_EQ(horizontal_from(vertical_align::justify | horizontal_align::justify), horizontal_align::justify);
+
+  EXPECT_EQ(only_vertical(horizontal_align::justify | vertical_align::top), align::top_left);
+  EXPECT_EQ(only_vertical(horizontal_align::justify | vertical_align::middle), align::middle_left);
+  EXPECT_EQ(only_vertical(horizontal_align::justify | vertical_align::bottom), align::bottom_left);
+
+  EXPECT_EQ(to_opposite(horizontal_align::justify), horizontal_align::justify);
+  EXPECT_EQ(to_opposite(vertical_align::justify), vertical_align::justify);
+
+  /// This part is based on implementation details
+  EXPECT_EQ(int(horizontal_align::justify) & int(vertical_align::justify), 0);
+}
