9a9e040e69ee3387007efa5c8f7c6be0a803806f
Ghassan <ghassan@ghassan.pl>
2023-07-11 01:00:30 -0400

    * added [interpolate] library that performs simple string interpolation for []-delimited substrings: * `interpolate_simple(str, func)` does a simple find and replace - anything withing braces (e.g. [bla]) will be replaced by func("bla") * `interpolate_eval(str, env)` is similar to the above, except that it will eval the string in braces using the given eval environment and substitute in the result

---
+++ include/ghassanpl/interpolate.h
@@ -0,0 +1,59 @@
+/// \copyright This Source Code Form is subject to the terms of the Mozilla Public
+/// License, v. 2.0. If a copy of the MPL was not distributed with this
+/// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include "eval.h"
+#include "sexps.h"
+#include "json_helpers.h"
+
+namespace ghassanpl
+{
+	template <typename FUNC>
+	std::string interpolate_simple(std::string_view str, FUNC&& func)
+	{
+		static_assert(std::invocable<FUNC, std::string_view> && std::constructible_from<std::string, std::invoke_result_t<FUNC, std::string_view>>, "function must take a stringable and return a string");
+		std::string result;
+		while (!str.empty())
+		{
+			result += string_ops::consume_until_delim(str, '[');
+			if (str.empty()) break;
+			if (string_ops::consume(str, '['))
+				result += '[';
+			else
+			{
+				auto key = string_ops::consume_until_delim(str, ']');
+				result += func(key);
+			}
+		}
+		return result;
+	}
+
+	template <bool SYNTAX>
+	std::string interpolate_eval(std::string_view str, eval_env<SYNTAX>& env)
+	{
+		std::string result;
+		while (!str.empty())
+		{
+			result += string_ops::consume_until(str, '[');
+			if (str.empty()) break;
+			str.remove_prefix(1);
+			if (string_ops::consume(str, '['))
+				result += '[';
+			else
+			{
+				auto call = formats::sexpressions::consume_list(str);
+				nlohmann::json call_result = env.eval(call.get_ref<nlohmann::json::array_t const&>());
+				formats::json::visit(call_result, [&](auto&& val) {
+					using std::to_string;
+					using nlohmann::to_string;
+					using ghassanpl::string_ops::to_string;
+					if constexpr (requires { { to_string(val) }; })
+						result += to_string(val);
+				});
+			}
+		}
+		return result;
+	}
+}
