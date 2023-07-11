62a11ea5b7c5dd5a97e9ebe21020ef867b51fc59
Ghassan <ghassan@ghassan.pl>
2023-07-11 01:16:09 -0400

    * added [uri], a uri parsing and constructing library * this version is extremely basic, it can basically only parse/normalize URIs (`decompose_uri`), but does it correctly, according to the URI RFC standard * notably does NOT validate host names or IPs as syntactically valid as of yet * has basic support for validating http(s) uris, but you shouldn't rely on it for anything except parsing * depends on tl-expected for now, until MSVC fixes its shit * split into uri.h and uri_impl.h for now, but should probably be spun-out as its own standalone non-header-only library

---
+++ include/ghassanpl/uri.h
@@ -0,0 +1,280 @@
+#pragma once
+
+#include <vector>
+#include <stdexcept>
+#include <system_error>
+
+#include <tl/expected.hpp> /// TODO: Until MSVC fixes its expected impl
+
+#include "enum_flags.h"
+#include "string_ops.h"
+#include "parsing.h"
+
+namespace ghassanpl
+{
+	/// https://github.com/austinsc/Poco/blob/master/Foundation/include/Poco/URI.h
+	/// https://docs.pocoproject.org/current/Poco.URI.html
+	/// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2012/n3420.html
+
+	/// URIs are stored in a UTF-8 encoding where both non-ASCII code unit bytes as well as URI-reserved characters (delimiters, etc) are %-encoded
+	using uri = std::string;
+	using uri_view = std::string_view;
+
+	enum class uri_error_code
+	{
+		no_error,
+
+		unknown_uri_scheme,
+
+		scheme_malformed,
+		scheme_invalid,
+		scheme_empty,
+
+		authority_malformed,
+		authority_empty,
+		authority_not_allowed_in_scheme,
+		authority_invalid_for_scheme,
+		authority_required_in_scheme,
+
+		user_info_malformed,
+		user_info_not_allowed_in_scheme,
+		user_info_invalid_for_scheme,
+		user_info_required_in_scheme,
+
+		host_malformed,
+		host_not_allowed_in_scheme,
+		host_invalid_for_scheme,
+		host_required_in_scheme,
+
+		port_malformed,
+		port_not_allowed_in_scheme,
+		port_invalid_for_scheme,
+		port_required_in_scheme,
+
+		path_malformed,
+		path_element_malformed,
+
+		query_malformed,
+		query_not_allowed_in_scheme,
+		query_invalid_for_scheme,
+		query_required_in_scheme,
+
+		fragment_malformed,
+		fragment_not_allowed_in_scheme,
+		fragment_invalid_for_scheme,
+		fragment_required_in_scheme,
+
+		invalid_percent_encoding,
+
+		no_scheme_specific_elements,
+		scheme_specific_element_malformed,
+	};
+
+	struct decomposed_uri
+	{
+		decomposed_uri() = default;
+		decomposed_uri(decomposed_uri const&) = default;
+		decomposed_uri(decomposed_uri&&) = default;
+		decomposed_uri& operator=(decomposed_uri const&) = default;
+		decomposed_uri& operator=(decomposed_uri&&) = default;
+
+		std::string scheme{};
+		std::string authority{};
+		std::string user_info{};
+		std::string host{};
+		std::string port{};
+		std::string path{};
+		std::vector<std::string> path_elements;
+		std::vector<std::string> normalized_path() const noexcept;
+		std::string query{};
+		std::vector<std::pair<std::string, std::string>> query_elements;
+		std::string fragment{};
+
+		bool canonical_form = false;
+
+		bool empty() const noexcept { return scheme.empty(); }
+
+		bool operator==(decomposed_uri const& other) const noexcept;
+	};
+
+	template <typename T>
+	using uri_expected = tl::expected<T, uri_error_code>;
+	using uri_error = std::optional<uri_error_code>; // TODO: MSVS has a bug where std::expected doesn't work with std::error_condition error types
+
+	/// <summary>
+	/// Removes data that should not be displayed to an untrusted user (user-info after the first ':', perhaps other things)
+	/// </summary>
+	/// <typeparam name="CHAR_TYPE"></typeparam>
+	uri_expected<uri> make_uri_safe_for_display(uri_view uri);
+
+	enum class uri_decompose_flags
+	{
+		split_query_elements,
+		split_path_elements,
+		use_well_known_port_numbers, ///< if a port is not specified in the uri, the result will guess the port based on the scheme
+		//convert_plus_to_space,
+		lowercase_when_appropriate,
+		normalize_path,
+	};
+
+	/// <summary>
+	/// This function will decompose URI into its composite elements, which includes percent-decoding all the elements.
+	/// </summary>
+	uri_expected<decomposed_uri> decompose_uri(uri_view uri, enum_flags<uri_decompose_flags> flags = enum_flags<uri_decompose_flags>::all());
+
+	enum class uri_compose_flags
+	{
+		path_leading_slash,
+		path_trailing_slash,
+		lowercase_when_appropriate,
+		normalize_path,
+		use_known_scheme,
+		//convert_space_to_plus
+	};
+
+	uri_expected<uri> compose_uri(decomposed_uri const& decomposed, enum_flags<uri_compose_flags> flags = enum_flags<uri_compose_flags>::all());
+
+	uri_expected<uri> normalize_uri(uri_view uri);
+
+	struct known_uri_scheme
+	{
+		virtual ~known_uri_scheme() noexcept = default;
+
+		virtual uri_error validate(uri_view uri) const noexcept
+		{
+			auto decomposed = decompose_uri(uri);
+			if (!decomposed) return decomposed.error();
+
+			if (decomposed.value().scheme != scheme()) return uri_error_code::scheme_invalid;
+
+			if (auto err = validate_authority(decomposed.value().authority)) return err;
+			if (auto err = validate_path(decomposed.value().path)) return err;
+			if (auto err = validate_query(decomposed.value().query)) return err;
+			if (auto err = validate_fragment(decomposed.value().fragment)) return err;
+
+			return {};
+		}
+
+		virtual std::string_view scheme() const noexcept = 0;
+
+		virtual uri_error validate_authority(std::string_view fragment) const noexcept
+		{
+			if (auto err = validate_user_info(fragment)) return err;
+			if (auto err = validate_host(fragment)) return err;
+			if (auto err = validate_port(fragment)) return err;
+			return {};
+		}
+		virtual uri_error validate_user_info(std::string_view element) const noexcept { return {}; }
+		virtual uri_error validate_host(std::string_view element) const noexcept { return {}; }
+		virtual uri_error validate_port(std::string_view element) const noexcept { return {}; }
+		virtual uri_error validate_path(std::string_view element) const noexcept { return {}; }
+		virtual uri_error validate_query(std::string_view element) const noexcept { return {}; }
+		virtual uri_error validate_fragment(std::string_view element) const noexcept { return {}; }
+
+		virtual std::string_view default_authority() const noexcept { return {}; }
+		virtual std::string_view default_user_info() const noexcept { return {}; }
+		virtual std::string_view default_host() const noexcept { return {}; }
+		virtual std::string_view default_port() const noexcept { return {}; }
+		virtual std::string_view default_path() const noexcept { return {}; }
+		virtual std::string_view default_query() const noexcept { return {}; }
+		virtual std::string_view default_fragment() const noexcept { return {}; }
+
+		virtual enum_flags<uri_decompose_flags> default_decompose_flags() const noexcept { return enum_flags<uri_decompose_flags>::all(); }
+		virtual enum_flags<uri_compose_flags> default_compose_flags() const noexcept { return enum_flags<uri_compose_flags>::all(); }
+
+		virtual std::vector<std::pair<std::string, std::string>> split_query_elements(std::string_view query) const noexcept;
+
+		virtual std::string normalize_authority(std::string_view element) const noexcept { return std::string{ element }; }
+		virtual std::string normalize_user_info(std::string_view element) const noexcept { return std::string{ element }; }
+		virtual std::string normalize_host(std::string_view element) const noexcept { return std::string{ element }; }
+		virtual std::string normalize_port(std::string_view element) const noexcept { return std::string{ element }; }
+		virtual std::string normalize_path(std::string_view element) const noexcept { return std::string{ element }; }
+		virtual std::string normalize_query(std::string_view element) const noexcept { return std::string{ element }; }
+		virtual std::string normalize_fragment(std::string_view element) const noexcept { return std::string{ element }; }
+
+		/// These function is used to call the `callback` function for each scheme-specific "element" of the uri.
+		/// For example, when decoding the data URI "data:text/plain;charset=UTF-8;page=21,the%20data:1234,5678", it will call callback with approximately these results:
+		/// ("scheme", "data")
+		/// ("media_type", "text/plain")
+		/// ("parameters", ";charset=UTF-8;page=21")
+		/// ("data", "the data:1234,5678")
+		/// 
+		/// The function will return the same errors decompose_uri if there is an issue decomposing the URI, as well as:
+		/// - no_scheme_specific_elements - if this known_scheme does not support scheme element iteration
+		/// - scheme_specific_element_malformed - if a scheme-specific element was malformed or missing when required
+		virtual uri_error_code iterate_scheme_elements(uri_view uri, std::function<bool(std::string_view element_name, std::string_view element_value)> callback) const noexcept
+		{
+			return uri_error_code::no_scheme_specific_elements;
+		}
+
+		uri_expected<uri> normalize_uri(uri_view uri);
+
+		virtual bool equivalent(uri_view u1, uri_view u2) const noexcept { return decompose_uri(u1) == decompose_uri(u2); }
+	};
+
+	known_uri_scheme const* query_uri_scheme(std::string_view scheme);
+
+	namespace known_schemes
+	{
+		struct file_scheme : public known_uri_scheme
+		{
+			/// https://datatracker.ietf.org/doc/html/rfc8089
+			virtual std::string_view scheme() const noexcept override { return "file"; }
+
+			static bool is_local(decomposed_uri const& uri)
+			{
+				return uri.host.empty() || uri.host == "localhost";
+			}
+		};
+		inline const file_scheme file;
+	}
+
+	struct uri_builder
+	{
+		uri_builder(uri& uri);
+		uri_builder(uri& uri, known_uri_scheme const& scheme);
+		uri_builder(const uri_builder&) = delete;
+		uri_builder& operator= (const uri_builder&) = delete;
+		~uri_builder();
+
+		template <class Source>
+		uri_builder& scheme(const Source& scheme);
+
+		template <class Source>
+		uri_builder& authority(const Source& authority);
+
+		template <class Source>
+		uri_builder& authority(const Source& user_info, const Source& host, const Source& port);
+
+		template <class Source>
+		uri_builder& user_info(const Source& user_info);
+
+		template <class Source>
+		uri_builder& host(const Source& host);
+
+		template <class Source>
+		uri_builder& port(const Source& port);
+
+		template <class Source>
+		uri_builder& path(const Source& path);
+
+		template <class Source>
+		uri_builder& query(const Source& query);
+
+		template <class Source>
+		uri_builder& fragment(const Source& fragment);
+
+	};
+
+	/// NOTE: https://www.iana.org/assignments/uri-schemes/uri-schemes.xhtml
+	/// NOTE: https://en.wikipedia.org/wiki/List_of_URI_schemes
+}
+
+
+namespace std
+{
+	template <>
+	struct is_error_code_enum<ghassanpl::uri_error_code> : true_type {};
+}
+
+#include "uri_impl.h"
---
+++ include/ghassanpl/uri_impl.h
@@ -0,0 +1,377 @@
+namespace ghassanpl
+{
+	namespace detail
+	{
+		using namespace string_ops;
+		using namespace parsing;
+
+		template <typename T>
+		void eat(uri_view& uri, T&& c, uri_error_code err)
+		{
+			if (!consume(uri, std::forward<T>(c)))
+				throw err;
+		}
+
+		static constexpr auto isscheme(char c) { return ascii::isalnum(c) || c == '+' || c == '-' || c == '.'; };
+		static constexpr auto isunreserved(char c) { return ascii::isalnum(c) || c == '-' || c == '.' || c == '_' || c == '~'; };
+		static constexpr auto isgendelims(char c) { return ascii::isany(c, ":/?#[]@"); };
+		static constexpr auto issubdelims(char c) { return ascii::isany(c, "!$&'()*+,;="); };
+		static constexpr auto isreserved(char c) { return ascii::isany(c, ":/?#[]@!$&'()*+,;="); };
+
+		template <typename T>
+		std::string condlower(T&& s, enum_flags<uri_decompose_flags> const flags)
+		{
+			return flags.contain(uri_decompose_flags::lowercase_when_appropriate) ? ascii::tolower(std::move(s)) : std::move(s);
+		}
+
+		static std::string parse_scheme(uri_view& uri, enum_flags<uri_decompose_flags> const flags)
+		{
+			const auto start = uri;
+			eat(uri, ascii::isalpha, uri_error_code::scheme_malformed);
+			trim_while(uri, isscheme);
+
+			return condlower(make_string(start.begin(), uri.begin()), flags);
+		}
+
+		static std::string parse_authority(uri_view& uri, enum_flags<uri_decompose_flags> const flags)
+		{
+			return std::string(consume_until(uri, [](char c) { return c == '/' || c == '?' || c == '#'; }));
+		}
+
+		static char parse_pct(std::string_view& str)
+		{
+			uint8_t value = 0;
+			if (auto h1 = consume(str, ascii::isxdigit))
+				value += ascii::xdigit_to_number(h1) * 16;
+			else
+				throw uri_error_code::invalid_percent_encoding;
+			if (auto h2 = consume(str, ascii::isxdigit))
+				value += ascii::xdigit_to_number(h2);
+			else
+				throw uri_error_code::invalid_percent_encoding;
+			return (char)value;
+		}
+
+		template <typename PRED>
+		static std::string consume_with_pct(std::string_view& str, PRED&& pred, std::string_view prefix = {})
+		{
+			std::string result{ prefix };
+			do
+			{
+				result += consume_while(str, pred);
+				if (consume(str, '%'))
+					result += parse_pct(str);
+				else
+					break;
+			} while (true);
+			return result;
+		}
+
+		static std::string try_parse_ipv4(std::string_view& str, enum_flags<uri_decompose_flags> const flags)
+		{
+			auto start = str;
+			auto [_, n1] = consume_c_unsigned(str);
+			if (n1 < 0 || n1 > 255) return {};
+			if (!consume(str, '.')) return {};
+			auto [__, n2] = consume_c_unsigned(str);
+			if (n2 < 0 || n2 > 255) return {};
+			if (!consume(str, '.')) return {};
+			auto [___, n3] = consume_c_unsigned(str);
+			if (n3 < 0 || n3 > 255) return {};
+			if (!consume(str, '.')) return {};
+			auto [____, n4] = consume_c_unsigned(str);
+			if (n4 < 0 || n4 > 255) return {};
+
+			return make_string(start.begin(), str.begin());
+		}
+
+		static std::string parse_host(std::string_view& authority, enum_flags<uri_decompose_flags> const flags)
+		{
+			if (consume(authority, '['))
+			{
+				/// IP-literal = "[" ( IPv6address / IPvFuture  ) "]"
+				/// IPvFuture  = "v" 1*HEXDIG "." 1*( unreserved / sub-delims / ":" )
+				/// 
+				/// Not checking validity of IP-literals for now
+
+				auto result = consume_until(authority, ']');
+				if (result.empty() || !consume(authority, ']'))
+					throw uri_error_code::host_malformed;
+				return std::string{ result };
+			}
+			else
+			{
+				auto start = authority;
+				auto ipv4 = try_parse_ipv4(authority, flags);
+				if (!ipv4.empty())
+					return ipv4;
+
+				/// Not an ipv4, try parsing as reg-name
+				authority = start;
+
+				/// Not checking validity of reg-names for now
+				return condlower(consume_with_pct(authority, [](char c) { return isunreserved(c) || c == '-' || c == '.'; }), flags);
+			}
+		}
+
+		static std::tuple<std::string, std::string, std::string> parse_authority_elements(std::string_view authority, enum_flags<uri_decompose_flags> const flags)
+		{
+			std::tuple<std::string, std::string, std::string> result;
+
+			if (authority.contains('@'))
+			{
+				std::get<0>(result) = consume_with_pct(authority, [](char c) { return isunreserved(c) || issubdelims(c) || c == ':'; }); /// user_info
+				eat(authority, '@', uri_error_code::authority_malformed);
+				std::get<1>(result) = parse_host(authority, flags);
+			}
+			else
+			{
+				std::get<1>(result) = parse_host(authority, flags); /// host
+			}
+
+			if (consume(authority, ':'))
+			{
+				std::get<2>(result) = std::string(consume_while(authority, ascii::isdigit));
+			}
+
+			return result;
+		}
+
+		static constexpr auto ispchar(char c) { return isunreserved(c) || issubdelims(c) || c == ':' || c == '@'; }
+		static constexpr auto ispcharnc(char c) { return isunreserved(c) || issubdelims(c) || c == '@'; }
+
+		static std::string parse_segment(uri_view& uri, enum_flags<uri_decompose_flags> const flags)
+		{
+			return consume_with_pct(uri, ispchar);
+		}
+
+		static std::string parse_segment_nonzero(uri_view& uri, enum_flags<uri_decompose_flags> const flags)
+		{
+			auto result = consume_with_pct(uri, ispchar);
+			if (result.empty())
+				throw uri_error_code::path_element_malformed;
+			return result;
+		}
+
+		static std::string parse_segment_nonzero_noncolon(uri_view& uri, enum_flags<uri_decompose_flags> const flags)
+		{
+			auto result = consume_with_pct(uri, ispcharnc);
+			if (result.empty())
+				throw uri_error_code::path_element_malformed;
+			return result;
+		}
+
+		static std::tuple<std::string, std::vector<std::string>> parse_path(bool with_authority, uri_view& uri, enum_flags<uri_decompose_flags> const flags)
+		{
+			/// If with_authority == true, the path component must either be empty or begin with a slash("/") character.
+			auto path = consume_until(uri, [](char c) { return c == '?' || c == '#'; });
+
+			if (with_authority && !path.empty() && path[0] != '/')
+				throw uri_error_code::path_malformed;
+
+			std::vector<std::string> elements;
+
+			if (!path.empty())
+			{
+				uri_view pv = path;
+
+				trim(pv, '/');
+
+				if (!pv.empty())
+				{
+					do
+					{
+						if (flags.contain(uri_decompose_flags::split_path_elements)) elements.push_back(parse_segment(pv, flags));
+					} while (consume(pv, '/'));
+
+					if (!pv.empty())
+						throw uri_error_code::path_malformed;
+				}
+			}
+
+			return { std::string{ path }, std::move(elements) };
+		}
+
+		static constexpr auto isqorf(char c) { return ispchar(c) || c == '/' || c == '?'; }
+
+		template <char QUERY_DELIMITER = '&', char KEY_DELIMITER = '='>
+		void split_query_elements(std::string_view path, std::vector<std::pair<std::string, std::string>>& elements)
+		{
+			split(path, QUERY_DELIMITER, [&elements](std::string_view el, bool last) {
+				auto k = consume_until(el, KEY_DELIMITER); std::ignore = consume(el);
+				elements.push_back({ std::string{k}, std::string{el} });
+				});
+		}
+
+		static std::tuple<std::string, std::vector<std::pair<std::string, std::string>>> parse_query(uri_view& uri, enum_flags<uri_decompose_flags> const flags)
+		{
+			std::tuple<std::string, std::vector<std::pair<std::string, std::string>>> result;
+
+			auto path = consume_with_pct(uri, isqorf, "?");
+			if (!uri.empty() && uri[0] != '#')
+				throw uri_error_code::query_malformed;
+
+			std::get<0>(result) = path; /// query begins with ? and that's been consumed so we back-pedal one character so that we are standard-compliant and include ? in the query
+			if (flags.contain(uri_decompose_flags::split_query_elements))
+				split_query_elements(std::string_view{ path }.substr(1), std::get<1>(result));
+
+			return result;
+		}
+
+		static std::string parse_fragment(uri_view& uri, enum_flags<uri_decompose_flags> const flags)
+		{
+			auto fragment = consume_with_pct(uri, isqorf, "#");
+			if (!uri.empty())
+				throw uri_error_code::query_malformed;
+			return fragment; /// fragment begins with # and that's been consumed so we back-pedal one char to be compliant
+		}
+
+		static std::string deduce_port_from_scheme(std::string_view scheme)
+		{
+			auto known = query_uri_scheme(scheme);
+			if (known)
+				return std::string{ known->default_port() };
+			return {};
+		}
+	}
+
+
+	uri_expected<decomposed_uri> decompose_uri(uri_view uri, enum_flags<uri_decompose_flags> const flags)
+	{
+		try
+		{
+			decomposed_uri result{};
+
+			result.scheme = detail::parse_scheme(uri, flags);
+
+			if (!string_ops::consume(uri, ':'))
+				return tl::unexpected(uri_error_code::scheme_malformed);
+
+			if (string_ops::consume(uri, "//"))
+			{
+				result.authority = detail::parse_authority(uri, flags);
+
+				auto [user_info, host, port] = detail::parse_authority_elements(result.authority, flags);
+				if (port.empty() && flags.contain(uri_decompose_flags::use_well_known_port_numbers))
+					port = detail::deduce_port_from_scheme(result.scheme);
+				result.user_info = std::move(user_info);
+				result.host = std::move(host);
+				result.port = std::move(port);
+			}
+
+			auto [path, elements] = detail::parse_path(!result.authority.empty(), uri, flags);
+			result.path = std::move(path);
+			result.path_elements = std::move(elements);
+			if (flags.contain(uri_decompose_flags::normalize_path))
+				result.path_elements = result.normalized_path();
+
+			if (string_ops::consume(uri, '?'))
+			{
+				auto [query, elements] = detail::parse_query(uri, flags);
+				result.query = std::move(query);
+				result.query_elements = std::move(elements);
+			}
+
+			if (string_ops::consume(uri, '#'))
+				result.fragment = detail::parse_fragment(uri, flags);
+
+			if (flags.are_all_set(uri_decompose_flags::lowercase_when_appropriate, uri_decompose_flags::normalize_path))
+				result.canonical_form = true;
+
+			return result;
+		}
+		catch (uri_error_code code)
+		{
+			return tl::unexpected(code);
+		}
+	}
+
+	uri_expected<uri> compose_uri(decomposed_uri const& decomposed, enum_flags<uri_decompose_flags> const flags)
+	{
+		return {};
+	}
+
+	bool decomposed_uri::operator==(decomposed_uri const& other) const noexcept
+	{
+		return
+			string_ops::ascii::strings_equal_ignore_case(scheme, other.scheme) &&
+			user_info == other.user_info &&
+			string_ops::ascii::strings_equal_ignore_case(host, other.host) &&
+			string_ops::trimmed(port, '0') == string_ops::trimmed(other.port, '0') &&
+			normalized_path() == other.normalized_path() &&
+			query == other.query &&
+			fragment == other.fragment;
+	}
+
+	std::vector<std::string> decomposed_uri::normalized_path() const noexcept
+	{
+		std::vector<std::string> result;
+		for (auto& el : path_elements)
+		{
+			if (el == ".")
+				continue;
+			else if (el == ".." && !result.empty())
+				result.pop_back();
+			else
+				result.push_back(el);
+		}
+		return result;
+	}
+
+	namespace known_schemes
+	{
+
+		struct url_schemes : known_uri_scheme
+		{
+			/// https://www.rfc-editor.org/rfc/rfc1738.html
+			virtual uri_error validate_host(std::string_view element) const noexcept override { if (element.empty()) return uri_error_code::host_required_in_scheme; return {}; }
+		};
+
+		struct http_schemes : url_schemes
+		{
+			virtual uri_error validate_user_info(std::string_view element) const noexcept override { return {}; }
+			virtual uri_error validate_path(std::string_view element) const noexcept override { if (!element.empty() || element.starts_with("/")) return uri_error_code::path_malformed; return {}; }
+
+			virtual std::string normalize_port(std::string_view element) const noexcept override { if (string_ops::trimmed(element, '0') == default_port()) return {}; return std::string{ element }; }
+			virtual std::string normalize_path(std::string_view element) const noexcept { if (element.empty()) return "/"; return std::string{ element }; }
+			virtual std::string normalize_host(std::string_view element) const noexcept { return string_ops::ascii::tolower(element); }
+		};
+
+		struct http_scheme : http_schemes
+		{
+			virtual std::string_view scheme() const noexcept override { return "http"; }
+			virtual std::string_view default_port() const noexcept override { return "80"; }
+		};
+
+		struct https_scheme : http_schemes
+		{
+			virtual std::string_view scheme() const noexcept override { return "https"; }
+			virtual std::string_view default_port() const noexcept override { return "443"; }
+		};
+
+		static http_scheme http;
+		static https_scheme https;
+
+		/// https://en.wikipedia.org/wiki/Data_URI_scheme
+
+	}
+
+	known_uri_scheme const* query_uri_scheme(std::string_view scheme)
+	{
+		static std::map<std::string, known_uri_scheme const*, std::less<>> const schemes = {
+			{"file", &known_schemes::file},
+			{"http", &known_schemes::http},
+			{"https", &known_schemes::https}
+		};
+		if (auto s = schemes.find(scheme); s != schemes.end())
+			return s->second;
+		return nullptr;
+	}
+
+	std::vector<std::pair<std::string, std::string>> known_uri_scheme::split_query_elements(std::string_view query) const noexcept
+	{
+		std::vector<std::pair<std::string, std::string>> result;
+		detail::split_query_elements(query, result);
+		return result;
+	}
+}
